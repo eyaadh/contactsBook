3119a869cfd1bd1aa085bac48fd504cb287ae919
eyaadh <eyaadh@gmail.com>
2020-08-13 19:11:43 +0600

    changes in cosmetics

--- book/google/__init__.py
+++ book/google/__init__.py
@@ -9,26 +9,26 @@ SCOPES = ['https://www.googleapis.com/auth/contacts.readonly']
 
 
 def google_auth():
-    creds = None
+    credentials = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists('book/working_dir/token.pickle'):
         with open('book/working_dir/token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+            credentials = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'book/working_dir/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            credentials = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('book/working_dir/token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+            pickle.dump(credentials, token)
 
-    service = build('people', 'v1', credentials=creds, cache_discovery=False)
+    service = build('people', 'v1', credentials=credentials, cache_discovery=False)
 
     return service
 
